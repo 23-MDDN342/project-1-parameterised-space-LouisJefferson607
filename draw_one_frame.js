a8664bfa3770569048de1db7f4c98dd180c2a6a7
LouisJefferson607 <100631217+LouisJefferson607@users.noreply.github.com>
2023-03-07 10:20:38 +1300

    Update draw_one_frame.js
    Made it an eye and changed color scheme

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,33 +1,50 @@
 let gridSize = 10;
 let circleRadius = 20;
-let colors = ["#f94144", "#f3722c", "#f8961e", "#f9c74f", "#90be6d", "#43aa8b", "#577590"];
+let colors = ["#e8edfa", "#d6e6f8", "#b4c2da", "#6f95a6", "#70725a"];
 let angle = 0;
-	
-	function draw_one_frame() {
-		background(255);
 
-		// Calculate a time-based value for the colorIndex
-		let colorIndex = floor(frameCount / 10) % colors.length;
-	  
-		// Loop through each row and column in the grid
-		for (let x = 0; x < width; x += gridSize) {
-		  for (let y = 0; y < height; y += gridSize) {
-	  
-			// Calculate the color for this circle
-			let color = colors[(colorIndex + int((x + y) / 40)) % colors.length];
-	  
-			// Draw the circle with the perpendicular color gradient line
-			noFill();
-			stroke(color);
-			strokeWeight(circleRadius);
-			push();
-			translate(x + gridSize / 2, y + gridSize / 2);
-			rotate(atan2(height / 2 - y, width / 2 - x));
-			ellipse(0, 0, circleRadius);
-			pop();
-		  }
-		}
-	  }
+function draw_one_frame() {
+  // Set the ellipse mode to RADIUS and stroke settings
+  ellipseMode(RADIUS);
+  stroke(0);
+  strokeWeight(1);
+
+  // Draw a purple ellipse in the background
+  noStroke();
+  fill("#b4c2da");
+  ellipse(width / 2, height / 2, width / 2, height / 2);
+
+  // Loop through each row and column in the grid
+  for (let x = 0; x < width; x += gridSize) {
+    for (let y = 0; y < height; y += gridSize) {
+      // Skip drawing circles within the hole in the center of the screen
+      if (dist(x, y, width / 2, height / 2) < width / 4) {
+        continue;
+      }
+
+      // Calculate the color for this circle
+      let colorIndex = floor(frameCount / 10) % colors.length;
+      let color = colors[(colorIndex + int((x + y) / 40)) % colors.length];
+
+      // Draw the circle with the perpendicular color gradient line
+      noFill();
+      stroke(color);
+      push();
+      translate(x + gridSize / 2, y + gridSize / 2);
+      rotate(atan2(height / 2 - y, width / 2 - x));
+      ellipse(0, 0, circleRadius, circleRadius);
+      pop();
+    }
+  }
+
+  // Draw a white border around the outside of the canvas
+  noFill();
+  stroke("#e8edfa");
+  strokeWeight(10);
+  rect(0, 0, width, height);
+}
+
+
 		/*
 	translate(width/2, height/2);
 	for(var i=0;i<15;i++){
